3a6f53d0a4f9592af6017ac24b670e9253db62fc
bw007 <mr.abdulaziz00791@gmail.com>
2024-07-19 10:46:38 +0500

    Build

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,12 @@
-import { handleScroll } from './parts/navbar';
-window.addEventListener('scroll', handleScroll);
-console.log(12);
+"use strict";
+function handleScroll() {
+    const navbar = document.querySelector(".header-nav");
+    if (window.scrollY > 100) {
+        navbar.classList.add("fixed");
+    }
+    else {
+        navbar.classList.remove("fixed");
+    }
+    console.log(navbar);
+}
+handleScroll();
