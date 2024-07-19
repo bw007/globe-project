61d14a292bf7f59884928353751a917f3656545d
bw007 <mr.abdulaziz00791@gmail.com>
2024-07-19 16:32:09 +0500

    Ts navbar scroll fixed method

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,11 +1,32 @@
-function handleScroll() {
-  const navbar = document.querySelector(".header-nav") as HTMLElement;
+// Navbar fixed method
+function handleScroll(): void {
+  const navbar = document.querySelector(".header-nav") as HTMLElement | null;
+  const header = document.querySelector(".header") as HTMLElement | null;
 
-  if (window.scrollY > 100) {
-    navbar.classList.add("fixed");
-  } else {
-    navbar.classList.remove("fixed");
+  if (navbar) {
+    if (window.scrollY > 200 && window.innerWidth > 520) {
+      navbar?.classList.add("header-nav__fixed");
+      header?.classList.add("pt-90");
+    } else if(window.scrollY > 200 && window.innerWidth < 520) {
+      navbar?.classList.add("header-nav__fixed");
+      header?.classList.add("pt-90");
+    } else {
+      navbar?.classList.remove("header-nav__fixed");
+      header?.classList.remove("pt-75", "pt-90");
+    }
   }
-  console.log(navbar);
 }
-handleScroll()
+
+// Debounce for navbar fixed method
+function debounce<T extends (...args: any[]) => void>(func: T, wait: number): T { 
+  let timeout: NodeJS.Timeout | null = null;
+  return function(this: any, ...args: Parameters<T>): void {
+    if (timeout !== null) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => func.apply(this, args), wait);
+  } as T;
+}
+
+const debouncedHandleScroll = debounce(handleScroll, 100);
+window.addEventListener("scroll", debouncedHandleScroll);
